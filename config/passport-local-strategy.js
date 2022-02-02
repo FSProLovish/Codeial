bd012428801f9b1be4f6077d15cb423feb4afa78
FSProLovish <lavishgarg006@gmail.com>
2022-02-02 19:08:36 +0530

    authentication using Passportjs

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -45,4 +45,20 @@ passport.deserializeUser(function (id, done) {
   });
 });
 
+// check if user is authenticated
+passport.checkAuthentication = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  return res.redirect("/users/sign-in");
+};
+
+passport.setAuthenticatedUser = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    // req.user contains the current signed in user from the session cookie and we are just sending this to locals
+    res.locals.user = req.user;
+  }
+  next();
+};
+
 module.exports = passport;
