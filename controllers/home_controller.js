d748af6514535ecc34dedfe5477de46bb5dae433
FSProLovish <lavishgarg006@gmail.com>
2022-02-13 09:48:52 +0530

    13-02-2022

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,18 @@
+const Post = require("../models/post");
+
 module.exports.home = function (req, res) {
   // console.log(req.cookies);
-  return res.render("home", {
-    title: "Welcome",
-  });
+  // Populate the post of each user
+  Post.find({})
+    .populate("user")
+    .exec(function (err, posts) {
+      if (err) {
+        console.log("error in fetching the posts from database");
+        return;
+      }
+      res.render("home", {
+        title: "Codeial | Home",
+        posts: posts,
+      });
+    });
 };
