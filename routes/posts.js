4179d10065eef54c512ca29685a99d09cf1a2ad4
FSProLovish <lavishgarg006@gmail.com>
2022-02-13 10:45:00 +0530

    13-02-2022

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const postController = require("../controllers/post_controller");
 
-router.post("/create-post", postController.create);
+router.post(
+  "/create-post",
+  passport.checkAuthentication,
+  postController.create
+);
 module.exports = router;
