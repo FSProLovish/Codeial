9f0e23fefcb364a8e8cac2b1dce6e48c3bd70f1f
FSProLovish <lavishgarg006@gmail.com>
2022-02-14 13:53:17 +0530

    14-02-2022

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,6 +4,12 @@ module.exports.home = function (req, res) {
   // Populate the post of each user
   Post.find({})
     .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
+    })
     .exec(function (err, posts) {
       if (err) {
         console.log("error in fetching the posts from database");
