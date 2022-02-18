cd93e294ddcf1369ab85d5ebe6b188f9c9da6450
FSProLovish <lavishgarg006@gmail.com>
2022-02-18 16:22:19 +0530

    18-02-2022

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,27 +1,25 @@
 const Post = require("../models/post");
 const User = require("../models/user");
 
-module.exports.home = function (req, res) {
-  // Populate the post of each user
-  Post.find({})
-    .populate("user")
-    .populate({
-      path: "comments",
-      populate: {
-        path: "user",
-      },
-    })
-    .exec(function (err, posts) {
-      if (err) {
-        console.log("error in fetching the posts from database");
-        return;
-      }
-      User.find({}, function (err, users) {
-        res.render("home", {
-          title: "Codeial | Home",
-          posts: posts,
-          all_users: users,
-        });
+module.exports.home = async function (req, res) {
+  try {
+    let posts = await Post.find({})
+      .populate("user")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user",
+        },
       });
+    let users = await User.find({});
+
+    res.render("home", {
+      title: "Codeial | Home",
+      posts: posts,
+      all_users: users,
     });
+  } catch (err) {
+    console.log("Error", err);
+    return;
+  }
 };
