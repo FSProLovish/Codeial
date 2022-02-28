6d07284a1d8be3dc120160c2fd0e2f22fb843bfb
FSProLovish <lavishgarg006@gmail.com>
2022-02-28 12:58:41 +0530

    28-02-2022

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -25,7 +25,6 @@ module.exports.update = async function (req, res) {
           if (user.avatar) {
             fs.unlinkSync(path.join(__dirname, "..", user.avatar));
           }
-
           // this is saving the path of the uploaded file into the avatar field in the user
           user.avatar = User.avatarPath + "/" + req.file.filename;
         }
