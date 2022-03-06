0b52f63fb30904a4213d5d2e3613b9c621fbd7bc
FSProLovish <lavishgarg006@gmail.com>
2022-03-06 15:40:44 +0530

    06-03-2022

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -2,14 +2,17 @@ const nodeMailer = require("../config/nodemailer");
 
 // this is another way of exporting a method
 exports.newComment = (comment) => {
-  console.log("inside comment mailer");
+  let htmlString = nodeMailer.renderTemplate(
+    { comment: comment },
+    "/comments/new_comment"
+  );
 
   nodeMailer.transporter.sendMail(
     {
       from: "lavishgarg006@gmail.com",
       to: comment.user.email,
       subject: "New Comment Published",
-      html: "<h1>Yup, your comment is now published!</h1>",
+      html: htmlString,
     },
     (err, info) => {
       if (err) {
